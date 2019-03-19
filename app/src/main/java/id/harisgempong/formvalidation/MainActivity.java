74ba3d8c80b6915dfe103b8bc5830944124a3c3d
Haris Miftahul Hudha <harismiftahulhudha@gmail.com>
2019-03-19 17:46:50 +0700

    adding indonesia language and simple example usage

--- app/src/main/java/id/harisgempong/formvalidation/MainActivity.java
+++ app/src/main/java/id/harisgempong/formvalidation/MainActivity.java
@@ -2,6 +2,8 @@ package id.harisgempong.formvalidation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -11,14 +13,20 @@ import id.harisgempong.harisformvalidation.model.Request;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText inputMath, inputEnglish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inputMath = findViewById(R.id.inputMath);
+        inputEnglish = findViewById(R.id.inputEnglish);
+    }
+
+    public void check(View view) {
         FormValidation formValidation = new FormValidation(this);
-        formValidation.addRequest(new Request("math", 40, "max:30", true));
-        formValidation.addRequest(new Request("english", 10, "min:15|max:30", true));
+        formValidation.addRequest(new Request("math", inputMath.getText().toString(), "max:30", true));
+        formValidation.addRequest(new Request("english", inputEnglish.getText().toString(), "min:15|max:30", true));
         formValidation.setOnValidateListener(new OnValidateListener() {
             @Override
             public void onValidate(boolean isSuccessful, ArrayList<String> validationMessages, FormValidation fr) {
